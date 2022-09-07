f1374289bff0840e965b09345dd906b0a6627b13
BlackH3art <jaroslaw.musielak@outlook.com>
2022-09-07 20:19:32 +0200

    fix: cors + x-powered-by

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: 'http://localhost:3000'
+  });
+
+  app.getHttpAdapter().getInstance().disable('x-powered-by');
+
+  await app.listen(process.env.PORT || 3500);
 }
 bootstrap();
