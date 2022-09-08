6cd0b97bbc83d84d9dd060f81aa1761926fb4651
BlackH3art <jaroslaw.musielak@outlook.com>
2022-09-08 12:57:28 +0200

    '/signature' endpoint + HashedMessageInterface

---
+++ src/interfaces/HashedMessageInterface.ts
@@ -0,0 +1,3 @@
+export interface HashedMessageInterface {
+  msg: string;
+}
--- src/whitelist/whitelist.controller.ts
+++ src/whitelist/whitelist.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Inject } from '@nestjs/common';
+import { WhitelistService } from './whitelist.service';
+
+import { HashedMessageInterface } from 'src/interfaces/HashedMessageInterface';
+
 
 @Controller('whitelist')
-export class WhitelistController {}
+export class WhitelistController {
+
+  constructor(
+    @Inject(WhitelistService) private whitelistService
+  ) {}
+
+
+  @Get('/signature')
+  getSignature(
+    @Body() hashedMessage: HashedMessageInterface
+  ): Promise<any> {
+    return this.whitelistService.signMessageForContract(hashedMessage);
+  }
+
+  
+}
