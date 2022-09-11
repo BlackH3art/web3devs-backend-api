130adcc538eb20758836d2ff082ae1e81ec1f27f
BlackH3art <jaroslaw.musielak@outlook.com>
2022-09-11 13:25:26 +0200

    check if address is whitelisted before signature

--- src/whitelist/whitelist.service.ts
+++ src/whitelist/whitelist.service.ts
@@ -1,28 +1,37 @@
+import { readFile } from 'fs/promises';
 import { Injectable } from '@nestjs/common';
 import { ethers } from 'ethers';
 
 import { HashedMessageInterface } from 'src/interfaces/HashedMessageInterface';
+import { Response } from 'express';
 
 @Injectable()
 export class WhitelistService {
 
 
-  async signMessageForContract(hashedMessage: HashedMessageInterface) {
+  async signMessageForContract(hashedMessage: HashedMessageInterface, address: string, res: Response) {
 
     const { msg } = hashedMessage;
 
     try {
+
+      const dataFile = await readFile('./src/data/whitelist.json', 'utf8');
+      const data = JSON.parse(dataFile);
       
-      const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
-      const sig = await wallet.signMessage(ethers.utils.arrayify(msg));
-  
-      return {
-        sig,
-      };
+      
+      if(data.whitelist.find(whitelistedAddres => address === whitelistedAddres)) {
+
+        const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+        const sig = await wallet.signMessage(ethers.utils.arrayify(msg));
+    
+        res.status(200).json({ whitelisted: true, sig: sig });
+      } else {
+        
+        res.status(404).json({ whitelisted: false, sig: '' });
+      }
 
     } catch (error) {
-      console.error(error);
-      return { error };
+      res.status(400).json({ whitelisted: false, error: true, msg: 'Problem with signature' });
     }
   }
 }
